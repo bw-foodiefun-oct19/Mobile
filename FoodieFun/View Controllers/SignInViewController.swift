0defbfef54b837a9b46aaab450a096a843176ded
John Kouris <jdkouris@gmail.com>
2019-10-18 08:51:42 -0700

    Added the SignInViewController and wired up the outlets and action

---
+++ FoodieFun/View Controllers/SignInViewController.swift
@@ -0,0 +1,36 @@
+//
+//  SignInViewController.swift
+//  FoodieFun
+//
+//  Created by John Kouris on 10/18/19.
+//  Copyright © 2019 Dongwoo Pae. All rights reserved.
+//
+
+import UIKit
+
+class SignInViewController: UIViewController {
+    
+    @IBOutlet var emailTextField: UITextField!
+    @IBOutlet var passwordTextField: UITextField!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func loginButtonTapped(_ sender: Any) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

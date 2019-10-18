b4d9908191a537975dd70f454917fc150098aeaf
John Kouris <jdkouris@gmail.com>
2019-10-17 20:15:50 -0700

    Added the data models for user and experience

---
+++ FoodieFun/Models/Experience.swift
@@ -0,0 +1,35 @@
+//
+//  Experience.swift
+//  FoodieFun
+//
+//  Created by John Kouris on 10/17/19.
+//  Copyright © 2019 Dongwoo Pae. All rights reserved.
+//
+
+import Foundation
+
+struct Experience: Codable {
+    var id: Int
+    var restaurantName: String
+    var restaurantType: String
+    var itemName: String
+    var itemPhoto: String
+    var foodRating: Int
+    var itemComment: String
+    var waitTime: String
+    var dateVisited: Date
+    var userID: Int
+    
+    enum CodingKeys: String, CodingKey {
+        case id
+        case restaurantName = "restaurant_name"
+        case restaurantType = "restaurant_type"
+        case itemName = "item_name"
+        case itemPhoto = "item_photo"
+        case foodRating = "food_rating"
+        case itemComment = "item_comment"
+        case waitTime = "wait_time"
+        case dateVisited = "date_visited"
+        case userID = "user_id"
+    }
+}

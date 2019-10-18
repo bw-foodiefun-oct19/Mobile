ca799ca262b7bccb4fbacf5bbaee7c9d5bd0bf7b
John Kouris <jdkouris@gmail.com>
2019-10-17 20:52:06 -0700

    Added the Token struct Added the APIController and the signUp method Added the SignUpViewController file and added the signUpButtonTapped action and the signUp method

---
+++ FoodieFun/Application/APIController.swift
@@ -0,0 +1,74 @@
+//
+//  APIController.swift
+//  FoodieFun
+//
+//  Created by John Kouris on 10/17/19.
+//  Copyright © 2019 Dongwoo Pae. All rights reserved.
+//
+
+import Foundation
+
+enum HTTPMethod: String {
+    case get = "GET"
+    case put = "PUT"
+    case post = "POST"
+    case delete = "DELETE"
+}
+
+enum NetworkError: Error {
+    case noAuth
+    case badAuth
+    case otherError
+    case badData
+    case noDecode
+}
+
+class APIController {
+    
+    private let baseURL = URL(string: "https://backend-foodie-fun.herokuapp.com/api/")!
+    var token: Token?
+    
+    //TODO: - fix to use result type instead of just networkError
+    func signUp(with user: User, completion: @escaping(Error?) -> Void) {
+        
+        let requestURL = baseURL
+            .appendingPathComponent("auth")
+            .appendingPathComponent("register")
+        
+        // Build the request
+        var request = URLRequest(url: requestURL)
+        request.httpMethod = HTTPMethod.post.rawValue
+        
+        // Tell the API that the body is in JSON format
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        let encoder = JSONEncoder()
+        
+        do {
+            let userJSON = try encoder.encode(user)
+            request.httpBody = userJSON
+        } catch {
+            completion(error)
+        }
+        
+        // Perform the request (data task)
+        URLSession.shared.dataTask(with: request) { (data, response, error) in
+            
+            // Handle errors
+            if let error = error {
+                NSLog("Error signing up user: \(error)")
+                completion(error)
+            }
+            
+            if let response = response as? HTTPURLResponse,
+                response.statusCode != 200 {
+                completion(error)
+            }
+            
+            // nil means there was no error, everything succeeded
+            completion(nil)
+        }.resume()
+        
+    }
+    
+}

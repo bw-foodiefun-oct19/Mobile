5826328bf02c771386ff81793dfe0ae2f1b76f4a
John Kouris <jdkouris@gmail.com>
2019-10-22 19:22:34 -0700

    Updated formatting and fixed the blank ExperienceRepresentation

--- FoodieFun/Application/APIController.swift
+++ FoodieFun/Application/APIController.swift
@@ -122,75 +122,71 @@ class APIController {
         }.resume()
     }
     
-    
-    
-    
     //FetchingExperience - GET - requried fields -> username, password and token
-            func fetchExperiences(completion: @escaping (Error?)-> Void) {
-               
-               guard let bearer = token else {
-                   print("there is no bearer for fetchingMeals")
-                   return
-               }
-               
-               
-            let instructorURL = self.baseURL.appendingPathComponent("meals")
-               
-               var request = URLRequest(url: instructorURL)
-               request.httpMethod = HTTPMethod.get.rawValue
-               request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-               request.addValue(bearer.token, forHTTPHeaderField: "Authorization")
-               
-               URLSession.shared.dataTask(with: request) { (data, response, error) in
-                   if let response = response as? HTTPURLResponse,
-                       response.statusCode != 200 {
-                       completion(error)
-                       return
-                   }
-                   
-                   if let error = error {
-                       completion(error)
-                       return
-                   }
-                   
-                   guard let data = data else {
-                       completion(error)
-                       return
-                   }
-                   
-                   let jsonDecoder = JSONDecoder()
-                   
-                   jsonDecoder.keyDecodingStrategy = .convertFromSnakeCase
-                   
-                   do {
-                       let allExperiences = try jsonDecoder.decode([ExperienceRepresentation].self, from: data)
-                       
-                       /*
-                       it seems like backend is not designed to combine all experiences (meals) from all users
-                       myMeals for each user
-                       let userID = self.bearer?.id
-                       let myMeals = fitnessClasses.filter{$0.id == userID}
-                       self.meals = myMeals
-                       */
-                       
-                       //all meals from all users
-                       self.experiences = allExperiences
+    func fetchExperiences(completion: @escaping (Error?)-> Void) {
         
-                       completion(nil)
-                   } catch {
-                       NSLog("Error decoding animal objects: \(error)")
-                       completion(error)
-                       return
-                   }
-                   }.resume()
-           }
+        guard let bearer = token else {
+            print("there is no bearer for fetchingMeals")
+            return
+        }
+        
+        let instructorURL = self.baseURL.appendingPathComponent("meals")
+        
+        var request = URLRequest(url: instructorURL)
+        request.httpMethod = HTTPMethod.get.rawValue
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.addValue(bearer.token, forHTTPHeaderField: "Authorization")
+        
+        URLSession.shared.dataTask(with: request) { (data, response, error) in
+            if let response = response as? HTTPURLResponse,
+                response.statusCode != 200 {
+                completion(error)
+                return
+            }
+            
+            if let error = error {
+                completion(error)
+                return
+            }
+            
+            guard let data = data else {
+                completion(error)
+                return
+            }
+            
+            let jsonDecoder = JSONDecoder()
+            
+            jsonDecoder.keyDecodingStrategy = .convertFromSnakeCase
+            
+            do {
+                let allExperiences = try jsonDecoder.decode([ExperienceRepresentation].self, from: data)
+                
+                /*
+                 it seems like backend is not designed to combine all experiences (meals) from all users
+                 myMeals for each user
+                 let userID = self.bearer?.id
+                 let myMeals = fitnessClasses.filter{$0.id == userID}
+                 self.meals = myMeals
+                 */
+                
+                //all meals from all users
+                self.experiences = allExperiences
+                
+                completion(nil)
+            } catch {
+                NSLog("Error decoding animal objects: \(error)")
+                completion(error)
+                return
+            }
+        }.resume()
+    }
     
     
     //Creating Experience - POST - requried fields -> item_name
     func createExperience(for itemName: String, restaurantName: String, restaurantType: String, itemPhoto: String, foodRating: Int, itemComment: String, waitTime: String, dateVisited: Date, completion:@escaping(Error?)->()) {
     
         //change id and userID in experience to be optional and id and userID here to be nil
-        let experience = ExperienceRepresentation(id: <#T##Int#>, restaurantName: <#T##String#>, restaurantType: <#T##String#>, itemName: <#T##String#>, itemPhoto: <#T##String#>, foodRating: <#T##Int#>, itemComment: <#T##String#>, waitTime: <#T##String#>, dateVisited: <#T##Date#>, userID: <#T##Int#>)
+        let experience = ExperienceRepresentation(id: 0, restaurantName: restaurantName, restaurantType: restaurantType, itemName: itemName, itemPhoto: itemPhoto, foodRating: foodRating, itemComment: itemComment, waitTime: waitTime, dateVisited: dateVisited, userID: 0)
         
         //POST
         let createExperienceURL = self.baseURL.appendingPathComponent("meals")
@@ -234,55 +230,55 @@ class APIController {
     }
     
     //Updating experience - PUT - meals/id# for this specifi meal
-        func updateExperience(for experience: ExperienceRepresentation, changeitemNameto: String, changerestaurantNameto: String, changerestaurantTypeto: String, changeitemPhototo: String, changefoodRatingto: Int?, changeitemCommentto: String, changewaitTimeto: String, changedateVisitedto: String, completion:@escaping (Error?)->Void) {
-            
-            //making sure passed meal exists in array of allMeals
-            guard let index = self.experiences.firstIndex(of: experience) else {return}
-            self.experiences[index].itemName = changeitemNameto
-    //        self.allMeals[index].restaurantName = changerestaurantNameto!
-    //        self.allMeals[index].restaurantType = changerestaurantTypeto!
-            //add more change update as needed
-            
-            //let updatedClass = fitnessClasses[index]
-            //PUT
-            let mealID = experience.id
-            
-            let updateExperienceURL = self.baseURL.appendingPathComponent("meals/\(mealID)")
-            
-            //creating its own json file for name change
-     
-            let params = ["item_name": changeitemNameto] as [String: Any]
-            let json = try! JSONSerialization.data(withJSONObject: params, options: .prettyPrinted)
-       
-            
-            guard let bearer = self.token else {
-                completion(NSError())
+    func updateExperience(for experience: ExperienceRepresentation, changeitemNameto: String, changerestaurantNameto: String, changerestaurantTypeto: String, changeitemPhototo: String, changefoodRatingto: Int?, changeitemCommentto: String, changewaitTimeto: String, changedateVisitedto: String, completion:@escaping (Error?)->Void) {
+        
+        //making sure passed meal exists in array of allMeals
+        guard let index = self.experiences.firstIndex(of: experience) else {return}
+        self.experiences[index].itemName = changeitemNameto
+        //        self.allMeals[index].restaurantName = changerestaurantNameto!
+        //        self.allMeals[index].restaurantType = changerestaurantTypeto!
+        //add more change update as needed
+        
+        //let updatedClass = fitnessClasses[index]
+        //PUT
+        let mealID = experience.id
+        
+        let updateExperienceURL = self.baseURL.appendingPathComponent("meals/\(mealID)")
+        
+        //creating its own json file for name change
+        
+        let params = ["item_name": changeitemNameto] as [String: Any]
+        let json = try! JSONSerialization.data(withJSONObject: params, options: .prettyPrinted)
+        
+        
+        guard let bearer = self.token else {
+            completion(NSError())
+            return
+        }
+        
+        var request = URLRequest(url: updateExperienceURL)
+        request.httpMethod = HTTPMethod.put.rawValue
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.addValue(bearer.token, forHTTPHeaderField: "Authorization")
+        
+        //request httpBody of our own json format
+        request.httpBody = json
+        
+        URLSession.shared.dataTask(with: request) { (_, response, error) in
+            if let response = response as? HTTPURLResponse,
+                response.statusCode != 200 {
+                completion(NSError(domain: "", code: response.statusCode, userInfo: nil))
                 return
             }
             
-            var request = URLRequest(url: updateExperienceURL)
-            request.httpMethod = HTTPMethod.put.rawValue
-            request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-            request.addValue(bearer.token, forHTTPHeaderField: "Authorization")
-            
-            //request httpBody of our own json format
-            request.httpBody = json
-
-            URLSession.shared.dataTask(with: request) { (_, response, error) in
-                if let response = response as? HTTPURLResponse,
-                    response.statusCode != 200 {
-                    completion(NSError(domain: "", code: response.statusCode, userInfo: nil))
-                    return
-                }
-                
-                if let error = error {
-                    print(error)
-                    completion(error)
-                    return
-                }
-                completion(nil)
-                }.resume()
-        }
+            if let error = error {
+                print(error)
+                completion(error)
+                return
+            }
+            completion(nil)
+        }.resume()
+    }
     
     //Delete
     func deleteExperience(for experience: ExperienceRepresentation, completion: @escaping (Error?) -> Void) {

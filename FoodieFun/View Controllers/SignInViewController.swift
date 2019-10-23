150dadba477f46e9187c4ea4b3c08710579823b4
John Kouris <jdkouris@gmail.com>
2019-10-22 20:42:10 -0700

    Added tapGestureRecognizer to dismiss the keyboard when user clicks away Removed commented out code

--- FoodieFun/View Controllers/SignInViewController.swift
+++ FoodieFun/View Controllers/SignInViewController.swift
@@ -63,15 +63,5 @@ class SignInViewController: UIViewController {
         emailTextField.text = UserDefaults.standard.string(forKey: "username") ?? ""
         passwordTextField.text = UserDefaults.standard.string(forKey: "password") ?? ""
     }
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }

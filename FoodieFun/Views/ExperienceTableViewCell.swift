0d354c30c2d240cb11879e45f7793f10d756b452
John Kouris <jdkouris@gmail.com>
2019-10-24 13:00:58 -0700

    Updated the cell design to match the prototype Using a random image for the background instead of the set picture

--- FoodieFun/Views/ExperienceTableViewCell.swift
+++ FoodieFun/Views/ExperienceTableViewCell.swift
@@ -12,11 +12,11 @@ class ExperienceTableViewCell: UITableViewCell {
     
     @IBOutlet weak var restaurantName: UILabel!
     @IBOutlet weak var dateVisitedLabel: UILabel!
-    @IBOutlet weak var priceLabel: UILabel!
     @IBOutlet weak var backgroundImageView: UIImageView!
     @IBOutlet var itemNameLabel: UILabel!
     
     let dateFormatter = DateFormatter()
+    
     
     var experience: Experience? {
         didSet {
@@ -30,7 +30,6 @@ class ExperienceTableViewCell: UITableViewCell {
         itemNameLabel.text = experience?.itemName
         let dateVisited = dateFormatter.string(from: experience?.dateVisited ?? Date())
         dateVisitedLabel.text = dateVisited
-        self
     }
 
     override func awakeFromNib() {
